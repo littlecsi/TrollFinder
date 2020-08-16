337b6921a4c9266c6513ee92f780c40d9e1e7136
Lian Choi <littlecsi27@gmail.com>
2020-08-16 21:47:23 +0900

    [new] - Initialised Application using Visual Studio

---
+++ TrollFinder/HelloWindowsDesktop.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <tchar.h>
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+);
+
+LRESULT CALLBACK WndProc(
+	_In_ HWND   hWnd,
+	_In_ UINT   message,
+	_In_ WPARAM wParam,
+	_In_ LPARAM lParam
+);
